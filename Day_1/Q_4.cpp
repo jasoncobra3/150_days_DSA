13e1f005e3ab0aeb1fc3a45e93a657a9a8b2d28e
Aniket <127662135+jasoncobra3@users.noreply.github.com>
2024-01-14 00:16:44 +0530

    Solve Q_4 with new solution

--- Day_1/Q_4.cpp
+++ Day_1/Q_4.cpp
@@ -5,17 +5,15 @@ int main(){
     int n;
     cout<<"Enter the number:";
     cin>>n;
-    int count=1;
-    while (i<=n)
-    {
+    int sum=n*n;
+    while(i<=n){
         int j=1;
         while(j<=n){
-            cout<< i*3-(3-j);
+            cout<<sum<<" ";
+             sum=sum-1;
             j=j+1;
-
         }cout<<endl;
         i=i+1;
 
     }
-    
 }
